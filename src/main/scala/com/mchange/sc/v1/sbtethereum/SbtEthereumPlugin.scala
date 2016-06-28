750fa314c1ab4f25f6fadf00e67d8f902e92b93b
Steve Waldman <swaldman@mchange.com>
2016-06-28 05:27:08 -0700

    Add several new ethereum tasks

--- src/main/scala/com/mchange/sc/v1/sbtethereum/SbtEthereumPlugin.scala
+++ src/main/scala/com/mchange/sc/v1/sbtethereum/SbtEthereumPlugin.scala
@@ -2,13 +2,24 @@ package com.mchange.sc.v1.sbtethereum
 
 import sbt._
 import sbt.Keys._
-import plugins.{JvmPlugin,InteractionServicePlugin}
+import sbt.plugins.{JvmPlugin,InteractionServicePlugin}
+import sbt.complete.DefaultParsers._
+import sbt.InteractionServiceKeys.interactionService
 
-import java.io.File
+import java.io.{BufferedInputStream,File,FileInputStream}
+
+import play.api.libs.json.Json
+
+import com.mchange.sc.v2.lang.borrow
 
 import com.mchange.sc.v1.consuela.ethereum._
 
+// XXX: provisionally, for now... but what sort of ExecutionContext would be best when?
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object SbtEthereumPlugin extends AutoPlugin {
+
+  private val BufferSize = 4096
 
   val ZeroEthAddress = (0 until 40).map(_ => "0").mkString("")
 
@@ -20,14 +31,29 @@ object SbtEthereumPlugin extends AutoPlugin {
 
     val ethGethKeystore = settingKey[File]("geth-style keystore directory from which V3 wallets can be loaded")
 
+    val ethTargetDir = settingKey[File]("Location in target directory where ethereum artifacts will be placed")
+
     val soliditySource      = settingKey[File]("Solidity source code directory")
     val solidityDestination = settingKey[File]("Location for compiled solidity code and metadata")
 
     val compileSolidity = taskKey[Unit]("Compiles solidity files")
 
-    val ethGethWallet = taskKey[Option[wallet.V3]]("Loads a V3 wallet from a geth keystore") 
+    val ethGethWallet = taskKey[Option[wallet.V3]]("Loads a V3 wallet from a geth keystore")
+
+    val ethGetCredential = taskKey[Option[String]]("Requests masked input of a credential (wallet passphrase or hex private key)")
+
+    val ethDefaultGasPrice = taskKey[Long]("Finds the current default gas price")
+
+    val ethNextNonce = taskKey[Long]("Finds the next nonce for the address defined by setting 'ethAddress'")
+
+    val ethLoadCompilations = taskKey[Map[String,jsonrpc20.Compilation.Contract]]("Loads compiled solidity contracts")
+
+    val ethLoadContractHex = inputKey[String]("Loads hex contract initialization code for a specified named contract")
+
+    val ethDeployContract= inputKey[String]("Deploys the specified named contract")
 
     lazy val ethDefaults : Seq[sbt.Def.Setting[_]] = Seq(
+
       ethJsonRpcVersion := "2.0",
       ethJsonRpcUrl     := "http://localhost:8545",
 
@@ -35,8 +61,10 @@ object SbtEthereumPlugin extends AutoPlugin {
 
       ethAddress := ZeroEthAddress,
 
+      ethTargetDir in Compile := (target in Compile).value / "ethereum",
+
       soliditySource in Compile      := (sourceDirectory in Compile).value / "solidity",
-      solidityDestination in Compile := target.value / "solidity",
+      solidityDestination in Compile := (ethTargetDir in Compile).value / "solidity",
 
       compileSolidity in Compile := {
         val log            = streams.value.log
@@ -44,9 +72,6 @@ object SbtEthereumPlugin extends AutoPlugin {
 
         val solSource      = (soliditySource in Compile).value
         val solDestination = (solidityDestination in Compile).value
-
-        // XXX: provisionally, for now... but what sort of ExecutionContext would be best when?
-        import scala.concurrent.ExecutionContext.Implicits.global
 
         doCompileSolidity( log, jsonRpcUrl, solSource, solDestination )
       },
@@ -56,8 +81,51 @@ object SbtEthereumPlugin extends AutoPlugin {
         (compile in Compile).value
       },
 
+      ethDefaultGasPrice := {
+        val log            = streams.value.log
+        val jsonRpcUrl     = ethJsonRpcUrl.value
+        doGetDefaultGasPrice( log, jsonRpcUrl )
+      },
+
+      ethNextNonce := {
+        val log            = streams.value.log
+        val jsonRpcUrl     = ethJsonRpcUrl.value
+        val last = doGetTransactionCount( log, jsonRpcUrl, EthAddress( ethAddress.value ), jsonrpc20.Client.BlockNumber.Pending )
+        last + 1
+      },
+
+      ethLoadCompilations := {
+        val dummy = (compileSolidity in Compile).value // ensure compilation has completed
+
+        val dir = (solidityDestination in Compile).value
+
+        def addContracts( addTo : Map[String,jsonrpc20.Compilation.Contract], name : String ) = {
+          val next = borrow( new BufferedInputStream( new FileInputStream( new File( dir, name ) ), BufferSize ) )( Json.parse( _ ).as[jsonrpc20.Result.eth.compileSolidity] )
+          addTo ++ next.compilations
+        }
+
+        dir.list.foldLeft( Map.empty[String,jsonrpc20.Compilation.Contract] )( addContracts )
+      },
+
       ethGethWallet := {
         clients.geth.KeyStore.walletForAddress( ethGethKeystore.value, EthAddress( ethAddress.value ) ).toOption
+      },
+
+      ethGetCredential := {
+        interactionService.value.readLine("Enter passphrase or hex private key: ", mask = true)
+      },
+
+      ethLoadContractHex := {
+        val contractName = (Space ~> ID).parsed
+        val contractsMap = ethLoadCompilations.value
+
+        contractsMap( contractName ).code
+      },
+
+      // TODO...
+      ethDeployContract := {
+        val hex = ethLoadContractHex.evaluated
+        hex
       }
     )
   }
--- src/main/scala/com/mchange/sc/v1/sbtethereum/package.scala
+++ src/main/scala/com/mchange/sc/v1/sbtethereum/package.scala
@@ -12,9 +12,10 @@ import java.io._
 
 import com.mchange.sc.v2.lang.borrow
 import com.mchange.sc.v2.concurrent._
+import scala.concurrent.duration.Duration
 
 
-import com.mchange.sc.v1.consuela.ethereum.jsonrpc20
+import com.mchange.sc.v1.consuela.ethereum.{jsonrpc20,EthAddress}
 
 import play.api.libs.json._
 
@@ -26,6 +27,17 @@ package object sbtethereum {
   // XXX: hardcoded
   private val SolidityWriteBufferSize = 1024 * 1024; //1 MiB
 
+  private def doWithJsonClient[T]( log : sbt.Logger, jsonRpcUrl : String )( operation : jsonrpc20.Client => T )( implicit ec : ExecutionContext ) : T = {
+    try {
+      borrow( new jsonrpc20.Client.Simple( new URL( jsonRpcUrl ) ) )( operation )
+    } catch {
+      case e : java.net.ConnectException => {
+        log.error( s"Failed to connect to JSON-RPC client at '${jsonRpcUrl}': ${e}" )
+        throw e
+      }
+    }
+  }
+
   private [sbtethereum] def doCompileSolidity( log : sbt.Logger, jsonRpcUrl : String, solSource : File, solDestination : File )( implicit ec : ExecutionContext ) : Unit = {
     def solToJson( filename : String ) : String = filename match {
       case SolFileRegex( base ) => base + ".json"
@@ -34,54 +46,55 @@ package object sbtethereum {
     // TODO XXX: check imported files as well!
     def changed( destinationFile : File, sourceFile : File ) : Boolean = (! destinationFile.exists) || (sourceFile.lastModified() > destinationFile.lastModified() )
 
-    try {
-      borrow( new jsonrpc20.Client.Simple( new URL( jsonRpcUrl ) ) ) { client =>
-        solDestination.mkdirs()
-        val files = (solSource ** "*.sol").get
+    doWithJsonClient( log, jsonRpcUrl ){ client =>
+      solDestination.mkdirs()
+      val files = (solSource ** "*.sol").get
 
-        val filePairs = files.map( file => ( file, new File( solDestination, solToJson( file.getName() ) ) ) ) // (sourceFile, destinationFile)
-        val compileFiles = filePairs.filter( tup => changed( tup._2, tup._1 ) )
+      val filePairs = files.map( file => ( file, new File( solDestination, solToJson( file.getName() ) ) ) ) // (sourceFile, destinationFile)
+      val compileFiles = filePairs.filter( tup => changed( tup._2, tup._1 ) )
 
-        val cfl = compileFiles.length
-        if ( cfl > 0 ) {
-          val mbS = if ( cfl > 1 ) "s" else ""
-          log.info( s"Compiling ${compileFiles.length} Solidity source${mbS} to ${solDestination}..." )
+      val cfl = compileFiles.length
+      if ( cfl > 0 ) {
+        val mbS = if ( cfl > 1 ) "s" else ""
+        log.info( s"Compiling ${compileFiles.length} Solidity source${mbS} to ${solDestination}..." )
 
-          val compileFuts = compileFiles.map { tup =>
-            val srcFile  = tup._1
-            val destFile = tup._2
-            borrow( Source.fromFile( srcFile )(Codec.UTF8) )( _.close() ){ source =>
-              val code = source.foldLeft("")( _ + _ )
-              client.eth.compileSolidity( code ).map( result => ( destFile, result ) )
-            }
+        val compileFuts = compileFiles.map { tup =>
+          val srcFile  = tup._1
+          val destFile = tup._2
+          borrow( Source.fromFile( srcFile )(Codec.UTF8) )( _.close() ){ source =>
+            val code = source.foldLeft("")( _ + _ )
+            client.eth.compileSolidity( code ).map( result => ( destFile, result ) )
           }
-          val failures = awaitAndGatherFailures( compileFuts )
-          val failureCount = failures.size
-          if ( failureCount > 0 ) {
-            log.error( s"compileSolidity failed. [${failureCount} failures]" )
-            failures.foreach {
-              case jf : jsonrpc20.Failure => log.error( jf.message )
-              case other                  => log.error( other.toString )
-            }
-            throw failures.head
-          } else {
-            import Json._
-            compileFuts.map { fut =>
-              fut.map {
-                case ( destFile, result ) => {
-                  borrow( new OutputStreamWriter( new BufferedOutputStream( new FileOutputStream( destFile ), SolidityWriteBufferSize ), Codec.UTF8.charSet ) )( _.write( stringify( toJson ( result ) ) ) )
-                }
+        }
+        val failures = awaitAndGatherFailures( compileFuts )
+        val failureCount = failures.size
+        if ( failureCount > 0 ) {
+          log.error( s"compileSolidity failed. [${failureCount} failures]" )
+          failures.foreach {
+            case jf : jsonrpc20.Failure => log.error( jf.message )
+            case other                  => log.error( other.toString )
+          }
+          throw failures.head
+        } else {
+          import Json._
+          compileFuts.map { fut =>
+            fut.map {
+              case ( destFile, result ) => {
+                borrow( new OutputStreamWriter( new BufferedOutputStream( new FileOutputStream( destFile ), SolidityWriteBufferSize ), Codec.UTF8.charSet ) )( _.write( stringify( toJson ( result ) ) ) )
               }
             }
           }
         }
       }
-    } catch {
-      case e : java.net.ConnectException => {
-        log.error( s"Failed to connect to JSON-RPC client at '${jsonRpcUrl}': ${e}" )
-        throw e
-      }
     }
+  }
+
+  private [sbtethereum] def doGetDefaultGasPrice( log : sbt.Logger, jsonRpcUrl : String )( implicit ec : ExecutionContext ) : Long = {
+    doWithJsonClient( log, jsonRpcUrl )( client => Await.result( client.eth.gasPrice(), Duration.Inf ).price )
+  }
+
+  private [sbtethereum] def doGetTransactionCount( log : sbt.Logger, jsonRpcUrl : String, address : EthAddress, blockNumber : jsonrpc20.Client.BlockNumber )( implicit ec : ExecutionContext ) : Long = {
+    doWithJsonClient( log, jsonRpcUrl )( client => Await.result( client.eth.getTransactionCount( address, blockNumber ), Duration.Inf ).count )
   }
 }
 
