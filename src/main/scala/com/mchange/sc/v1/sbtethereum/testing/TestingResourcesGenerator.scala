4db8230787ac58ff87d7e54d5b3e7484889f7ce7
Steve Waldman <swaldman@mchange.com>
2017-05-23 22:48:26 -0700

    Fix testing.TestingResourcesGenerator

--- src/main/scala/com/mchange/sc/v1/sbtethereum/testing/TestingResourcesGenerator.scala
+++ src/main/scala/com/mchange/sc/v1/sbtethereum/testing/TestingResourcesGenerator.scala
@@ -9,7 +9,7 @@ import com.mchange.v2.io.IndentedWriter
 import com.mchange.sc.v1.consuela.ethereum.EthPrivateKey
 
 object TestingResourcesGenerator {
-  def generateTestingResources( objectName : String, testEthJsonRpcUrl : String, fountainKey : EthPrivateKey, fullyQualifiedPackageName : String ) : String = {
+  def generateTestingResources( objectName : String, testEthJsonRpcUrl : String, faucetKey : EthPrivateKey, fullyQualifiedPackageName : String ) : String = {
     val sw = new StringWriter()
 
     borrow( new IndentedWriter( sw ) ) { iw =>
@@ -19,47 +19,27 @@ object TestingResourcesGenerator {
       iw.println( "import com.mchange.sc.v1.consuela.ethereum.{EthKeyPair,EthPrivateKey}" )
       iw.println( "import com.mchange.sc.v1.consuela.ethereum.ethabi.stub" )
       iw.println( "import com.mchange.sc.v1.consuela.ethereum.jsonrpc20.Invoker" )
-      iw.println( "import com.mchange.sc.v1.consuela.ethereum.specification.Denominations" )
       iw.println()
 
       iw println( s"object ${objectName} {" )
       iw.upIndent()
 
       iw.println( s"""val EthJsonRpcUrl = "${testEthJsonRpcUrl}"""" )
-      iw.println( s"""val Fountain = EthKeyPair( EthPrivateKey( "0x${fountainKey.hex}" ) )""" )
+      iw.println( s"""val Faucet = EthKeyPair( EthPrivateKey( "0x${faucetKey.hex}" ) )""" )
       iw.println()
-      iw.println( s"""val DefaultSender = stub.Sender.Basic( Fountain )""" )
+      iw.println( s"""val DefaultSender = stub.Sender.Basic( Faucet )""" )
       iw.println()
 
       iw.println( "val EntropySource = new java.security.SecureRandom()" )
       iw.println()
 
-      iw.println( "trait Utilities extends Denominations {" )
-      iw.upIndent()
-
-      iw.println( "def awaitTransactionReceipt( transactionHash : EthHash, timeout : Duration )( implicit icontext : Invoker.Context, econtext : ExecutionContext ) : Option[ClientTransactionReceipt] = {" )
-      iw.upIndent()
-      iw.println( "Invoker.awaitTransactionReceipt( transactionHash, duration )" )
-      iw.downIndent()
-      iw.println( "}" )
-
-      iw.println( "def requireTransactionReceipt( transactionHash : EthHash, timeout : Duration = Duration.Inf )( implicit icontext : Invoker.Context, econtext : ExecutionContext ) : ClientTransactionReceipt = {" )
-      iw.upIndent()
-      iw.println( "Invoker.requireTransactionReceipt( transactionHash, duration )" )
-      iw.downIndent()
-      iw.println( "}" )
-
-      iw.println( "def createRandomSender() : stub.Sender = stub.Sender.Basic( EthKeyPair( EthPrivateKey( EntropySource ) ) )" )
-
-      iw.downIndent()
-      iw.println( "}" )
-
-
-      iw.println( "trait Context extends Utilities {" )
+      iw.println( "trait Context extends stub.Utilities {" )
       iw.upIndent()
 
       iw.println( "implicit val econtext = scala.concurrent.ExecutionContext.Implicits.global" )
-      iw.println( "implicit val icontext = Invoker.Context( ${testEthJsonRpcUrl}, ${Invoker.Markup( Default.GasMarkup )}, ${Invoker.Markup( Default.GasPriceMarkup )} )" )
+      iw.println( s"implicit val icontext = Invoker.Context( EthJsonRpcUrl, Invoker.Markup( ${Default.GasMarkup} ), Invoker.Markup( ${Default.GasPriceMarkup} ) )" )
+
+      iw.println( "def createRandomSender() : stub.Sender = stub.Sender.Basic( EthKeyPair( EthPrivateKey( EntropySource ) ) )" )
 
       iw.downIndent()
       iw.println( "}" )
--- src/main/scala/com/mchange/sc/v1/sbtethereum/testing/package.scala
+++ src/main/scala/com/mchange/sc/v1/sbtethereum/testing/package.scala
@@ -1,5 +1,7 @@
 package com.mchange.sc.v1.sbtethereum
 
+import com.mchange.sc.v1.consuela.ethereum.{EthKeyPair,EthPrivateKey}
+
 import com.mchange.sc.v1.consuela.ethereum.specification.Types.Unsigned256
 
 package object testing {
@@ -8,7 +10,16 @@ package object testing {
   final object Default {
     val EthJsonRpcPort = 58545 // conventional default, for testing
 
-    val TestRpcCommand = s"""testrpc --port ${EthJsonRpcPort} --account="0x${KeyPair.Fountain.pvt.hex},${MaxWei}""""
+    /**
+      * This is just a conventional account to use as an Ether fountain in testing environments
+      *
+      * Corresponds to eth address 0xaba220742442621625bb1160961d2cfcb64c7682
+      */
+    val Faucet = EthKeyPair( EthPrivateKey( BigInt( 0x7e57 ) ) )
+
+    val TestRpcCommand = s"""testrpc --port ${EthJsonRpcPort} --account="0x${Faucet.pvt.hex},${MaxWei}""""
+
+    val GasMarkup = 0.2
+    val GasPriceMarkup = 0
   }
 }
-
