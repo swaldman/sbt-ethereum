00123f0f06a8059ab8313d615149d121a04ba935
Steve Waldman <swaldman@mchange.com>
2018-05-29 14:40:09 -0700

    Define a more readable, numbers-and-digits-only timestamp to embed in backup file names.

---
+++ src/main/scala/com/mchange/sc/v1/sbtethereum/repository/util/InFilenameTimestamp.scala
@@ -0,0 +1,13 @@
+package com.mchange.sc.v1.sbtethereum.repository.util
+
+import java.time.{Instant,ZoneId}
+import java.time.format.DateTimeFormatter
+
+object InFilenameTimestamp {
+  private val Pattern   = "yyyyMMdd'T'HH'h'mm'm'ss's'SSS'ms'zzz"
+  private val Formatter = DateTimeFormatter.ofPattern( Pattern ).withZone( ZoneId.systemDefault() )
+
+  def generate( instant : Instant ) : String = Formatter.format( instant )
+
+  def parse( s : String ) : Instant = Instant.from( Formatter.parse( s ) ) 
+}
