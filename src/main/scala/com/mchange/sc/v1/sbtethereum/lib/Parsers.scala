4f77c802245bb7e4fbbe236d4fdaffbbaca7ff71
Steve Waldman <swaldman@mchange.com>
2018-12-23 04:51:32 -0800

    Remove convenience overload that made resolution ambiguous.

--- src/main/scala/com/mchange/sc/v1/sbtethereum/lib/Parsers.scala
+++ src/main/scala/com/mchange/sc/v1/sbtethereum/lib/Parsers.scala
@@ -8,8 +8,14 @@ import com.mchange.sc.v1.consuela.ethereum.EthAddress
 import com.mchange.sc.v1.sbtethereum.util
 
 object Parsers {
+  def addressParser( addressTabHelp : String, mbRpi : Option[RichParserInfo] ) : Parser[EthAddress] = {
+    util.Parsers.createAddressParser( addressTabHelp, mbRpi )
+  }
+  def parserGenerator[T]( op : Option[RichParserInfo] => Parser[T] ) : ( State, Option[RichParserInfo] ) => Parser[T] = { ( state, mbRpi ) =>
+    op( mbRpi )
+  }
   def parserGeneratorForAddress[T]( addressTabHelp : String )( op : Parser[EthAddress] => Parser[T] ) : ( State, Option[RichParserInfo] ) => Parser[T] = { ( state, mbRpi ) =>
-    op( util.Parsers.createAddressParser( addressTabHelp, mbRpi ) )
+    op( addressParser( addressTabHelp, mbRpi ) )
   }
 }
 
